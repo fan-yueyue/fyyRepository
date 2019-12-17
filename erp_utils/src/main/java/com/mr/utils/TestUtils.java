9650e36735eb9feb06c5c82d6cd3ca921d53e3f1
fanyueyue <1058442254@qq.com>
2019-12-17 22:46:45 +0800

    更新文件

--- erp_utils/src/main/java/com/mr/utils/TestUtils.java
+++ erp_utils/src/main/java/com/mr/utils/TestUtils.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TestUtils {
-    public static String testErp(){
+    public static String testErp() {
+
+        System.out.println("111111");
         return "nihao";
     }
 }
