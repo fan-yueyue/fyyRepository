c8fdc82af7197a23cef647aa6d0de1eeea2a34f4
fanyueyue <1058442254@qq.com>
2019-12-17 22:36:02 +0800

    mvn-repo-src

--- erp_product/src/main/java/com/mr/erp_product/ErpProductApplication.java
+++ erp_product/src/main/java/com/mr/erp_product/ErpProductApplication.java
@@ -2,10 +2,12 @@ package com.mr.erp_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@ComponentScan("com.mr")
 public class ErpProductApplication {
 
     public static void main(String[] args) {
